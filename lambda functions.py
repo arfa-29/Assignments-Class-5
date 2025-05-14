618c487a7708abbec342bf0b7fea40db30431c6d
arfa-29 <arfaasif1@gmail.com>
2025-05-15 01:23:55 +0500

    Add files via upload

---
+++ lambda functions.py
@@ -0,0 +1,13 @@
+# Write lambda functions for:
+
+# Squaring a number
+square = lambda a :(a**2)
+print(square(3))
+
+# Multiplying two numbers
+multiply = lambda a,b:(a*b)
+print(multiply(5,6))
+
+# Checking if a number is positive
+num_positive = lambda number: "Positive Number" if number >=0 else "Negative Number"
+print(num_positive(-6))
